334de9f04ec388af48463e2f9f7972494b69d51d
sh-ehio <shanice.ehio@gmail.com>
2024-10-01 13:57:55 +0200

    finish frontend

--- frontend/src/components/EditorMonaco.tsx
+++ frontend/src/components/EditorMonaco.tsx
@@ -4,11 +4,21 @@ import styles from "./styles/EditorMonaco.module.css";
 const EditorMonaco = () => {
   return (
     <div className={styles.main}>
-      <Editor
-        height="90vh"
-        defaultLanguage="javascript"
-        defaultValue="// some comment"
-      />
+      <Editor height="40vh" defaultLanguage="javascript" defaultValue="" />
+      <div className={styles.interaction}>
+        <div className={styles.selectorholder}>
+          <select className={styles.select_input}>
+            <option>HTML</option>
+            <option>Javascript</option>
+          </select>
+
+          <select className={styles.select_input_colortheme}>
+            <option>Light</option>
+            <option>Dark</option>
+          </select>
+        </div>
+        <button className={styles.btn}>Share</button>
+      </div>
     </div>
   );
 };
