002eb087548dc8033f00784f36d22519b0a6e7e2
sh-ehio <shanice.ehio@gmail.com>
2024-10-01 14:38:40 +0200

    finish editor

--- frontend/src/components/EditorMonaco.tsx
+++ frontend/src/components/EditorMonaco.tsx
@@ -1,23 +1,47 @@
 import Editor from "@monaco-editor/react";
 import styles from "./styles/EditorMonaco.module.css";
+import { useState } from "react";
+import ShareIcon from "../assets/Share.svg";
 
 const EditorMonaco = () => {
+  const [language, setLanguage] = useState("javascript");
+  const [theme, setTheme] = useState("light");
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setLanguage(e.target.value.toLowerCase());
+    console.log(e.target.value.toLowerCase());
+  };
+
+  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setTheme(e.target.value.toLowerCase());
+  };
+
   return (
     <div className={styles.main}>
-      <Editor height="40vh" defaultLanguage="javascript" defaultValue="" />
+      <Editor
+        height="40vh"
+        defaultLanguage="javascript"
+        language={language}
+        defaultValue="// some comment"
+        foreground={theme === "dark" ? "vs-dark" : "vs"}
+      />
       <div className={styles.interaction}>
         <div className={styles.selectorholder}>
-          <select className={styles.select_input}>
-            <option>HTML</option>
-            <option>Javascript</option>
-          </select>
-
-          <select className={styles.select_input_colortheme}>
-            <option>Light</option>
-            <option>Dark</option>
+          <select
+            className={styles.select_input}
+            onChange={handleSelect}
+            value={language}
+          >
+            <option value="javascript">javascript</option>
+            <option value="html">HTML</option>
+            <option value="typescript">typescript</option>
+            <option value="css">CSS</option>
+            <option value="json">JSON</option>
           </select>
         </div>
-        <button className={styles.btn}>Share</button>
+        <button className={styles.btn}>
+          <img src={ShareIcon}></img>Share
+        </button>
       </div>
     </div>
   );
